a1ae18fb78d1b5aa87377545daab0abf7b7a21e9
anirudhi89 <anirudh.iyer@outlook.com>
2023-12-24 00:06:07 -0800

    Initial commit

---
+++ src/main/java/com/example/BitemporalORM/BitemporalOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.BitemporalORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitemporalOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitemporalOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BitemporalORM/BitemporalOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BitemporalORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitemporalOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
